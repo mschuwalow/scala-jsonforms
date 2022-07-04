b6308708c07f7a19fd151a4bd90ce7554e840d4d
Maxim Schuwalow <16665913+mschuwalow@users.noreply.github.com>
2022-07-04 12:54:25 +0200

    Update scala-library to 2.12.16 (#76)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildHelper {
 
   val Scala211: String = "2.11.12"
-  val Scala212: String = "2.12.13"
+  val Scala212: String = "2.12.16"
   val Scala213: String = "2.13.6"
 
   def stdSettings(prjName: String) = Seq(
